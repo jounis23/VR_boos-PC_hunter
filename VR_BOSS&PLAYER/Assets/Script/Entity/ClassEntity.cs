cc6682bee506346f0010835676642502f3f0e311
yonghui yu <dydgml1935@gmail.com>
2020-04-20 15:57:29 +0900

    애니메이션 추가 및 이펙트

---
+++ VR_BOSS&PLAYER/Assets/Script/Entity/ClassEntity.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassEntity : MonoBehaviour
+{
+    public ClassName className;
+
+    public GameObject[] skillEffect;
+    public Transform[] skillTransfrom;
+    public enum ClassName
+    {
+        Archer,
+        Night,
+        Sorceress
+    }
+
+    public virtual void Attack(Entity entity)
+    {
+
+    }
+
+
+    public virtual void Skill1(Entity entity)
+    {
+
+    }
+
+    public virtual void Skill2(Entity entity)
+    {
+
+    }
+
+    public virtual void Skill3(Entity entity)
+    {
+
+    }
+
+    public virtual void Skill4(Entity entity)
+    {
+
+    }
+
+    public IEnumerator SkillEffectManage(GameObject skillEffect, Transform skillTransfrom)
+    {
+        GameObject skill= Instantiate(skillEffect, skillTransfrom);
+        yield return new WaitForSeconds(10.0f);
+        Destroy(skill);
+
+    }
+}
